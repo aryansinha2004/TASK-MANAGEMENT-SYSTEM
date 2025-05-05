90d2d203dce6de625ea40f36d0e6afd7f5934734
aryansinha2004 <hi.aryansinha@gmail.com>
2025-05-05 23:30:45 +0530

    Upload full project files

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    colors: {
+      primary: "#bb86fc",
+      secondary: "#121212",
+      tertiary: "#ffffff",
+      quartenary: "#03dac6",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
