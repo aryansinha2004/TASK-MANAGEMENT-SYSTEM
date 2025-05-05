bbd4141b4121d895f036fac642031f9fdb2aa26f
aryansinha2004 <113328970+aryansinha2004@users.noreply.github.com>
2025-05-06 00:49:38 +0530

    Update client.mjs

--- controllers/client.mjs
+++ controllers/client.mjs
@@ -2,7 +2,7 @@ import Member from "../models/member.mjs";
 
 export const getHome = (req, res, next) => {
     res.render("client/home", {
-        docTitle: "Tasksphere",
+        docTitle: "Taskmac",
         path: "/",
         projects: ""
     });
